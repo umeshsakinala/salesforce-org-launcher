f2a84d07bd09c0a92f82f2f585fa3644a5fc94ae
Umesh Chandra <umesh.sakinala20@gmail.com>
2025-10-28 17:47:51 +0530

    CORS issue fix
    Signed-off-by: Umesh Chandra <umesh.sakinala20@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "https://salesforce-orgs-launcher.onrender.com",
+  "https://salesforce-org-launcher.onrender.com",
 ];
 
 app.use(
